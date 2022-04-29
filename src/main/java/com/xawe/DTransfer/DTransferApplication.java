b77824a158f539c4c6a79f67ddcc1c512977c3e1
Xawe <dreamxawe@hotmail.com>
2022-04-29 18:05:51 -0300

    init

---
+++ src/main/java/com/xawe/DTransfer/DTransferApplication.java
@@ -0,0 +1,13 @@
+package com.xawe.DTransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xawe/DTransfer/DTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xawe.DTransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
