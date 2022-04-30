aaaf1c648713d24730b240c915bc7ecdbc962eb7
Xawe <dreamxawe@hotmail.com>
2022-04-30 11:15:36 -0300

    inlcuido comentario descrevendo a utilização do método execute do DTransfer

--- src/main/java/com/xawe/DTransfer/controller/DtransferController.java
+++ src/main/java/com/xawe/DTransfer/controller/DtransferController.java
@@ -1,4 +1,4 @@
-package com.xawe.dtransfer.model;
+package com.xawe.dtransfer.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/dtransfer", produces = "application/json;charset=UTF-8;")
 public class DtransferController {
     
+    /**
+     * Endpoint responsável por sensibilzar o Dtransfer, executando novos fluxos e fluxos pendentes.
+     * Operações:
+     * 1 - Identifica se há novos fluxos e cadastra no banco com status de pronto para execução.
+     * 2 - Executa todos os fluxos com status de Pronto para Execução seguindo a ordem descrita no campo Identifier
+     * 3 - Marca os fluxos executados como Sucesso ou Erro, prevenindo futuras execuções
+     * @return
+     */
     @GetMapping(value = "/execute")
     public ResponseEntity<String> executeDTransfer(){
         return ResponseEntity.ok().body("Sucess");
